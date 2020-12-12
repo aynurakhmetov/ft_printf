149374fa4d5099a7e2c610e84b43b3869fd05cb0
Gilda Marva <gmarva@et-f2.kzn.21-school.ru>
2020-12-12 17:04:58 +0300

    working version

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gmarva <gmarva@student.21-school.ru>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/05/08 17:10:17 by gmarva            #+#    #+#             */
-/*   Updated: 2020/08/13 20:13:05 by gmarva           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 size_t	ft_strlen(const char *str)
@@ -17,9 +5,9 @@ size_t	ft_strlen(const char *str)
 	size_t counter;
 
 	counter = 0;
-	if (!str)
-		return (0);
 	while (str[counter] != '\0')
+	{
 		counter++;
+	}
 	return (counter);
 }
